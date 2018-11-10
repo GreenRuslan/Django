b39b7f3e26fcc79a12fcc7af222214aebd09dc01
Ruslan <greenruslan@gmail.com>
2018-11-10 16:36:34 +0200

    I don't remember what I have changed

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+from .models import BlogPost
+
+def allblogs(request):
+    blogs  = BlogPost.objects
+    return render(request, 'blog/allblogs.html', {'blogs':blogs})
+
+def detail(request, blog_id):
+    detailblog =  get_object_or_404(BlogPost, pk=blog_id)
+    return render(request, 'blog/detail.html', {'blog':detailblog})
